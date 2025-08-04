ae8ab170b45f8a8d9aae39e3c1639d83de62e310
Malik Qattoum <malikqattom@gmail.com>
2025-08-04 18:31:02 +0300

    Remove Livewire scripts from master layout
    This commit removes the @livewireScripts directive from the master.blade.php template, likely to optimize page load performance or because Livewire components are no longer needed on all pages. The change affects the global JavaScript includes while preserving other script stacks.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -998,7 +998,6 @@
     <script src="{{ asset('js/plugins.min.js') }}"></script>
     <script src="{{ asset('js/functions.bundle.js') }}"></script>
 
-    @livewireScripts
     @stack('scripts')
 
 </body>
